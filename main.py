ea7dbc14cf74cc5b936eb361a2c679d11a9cc445
Marcos Vinicius <sbmarcos777@gmail.com>
2021-04-22 21:17:58 -0300

    todas palavras arrumadas.

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 
 
 # função para escrever em um arquivo json (olhar na pasta do seu projeto, será criado um
-# arquivo .json com o nome da tabela passada por parÃ¢metro)
+# arquivo .json com o nome da tabela passada por parâmetro)
 def escrever_json(data, file_name):
     with open(file_name + '.json', 'w') as f:
         json.dump(data, f, indent=4)
         f.close()
 
 
-# funÃ§Ã£o para ler em um arquivo json e colocar em memÃ³ria
+# função para ler em um arquivo json e colocar em memória
 def ler_json(file_name):
     data = {}
     try:
@@ -22,7 +22,7 @@ def ler_json(file_name):
         return data
 
 
-# funÃ§Ã£o para criar um novo registro em um arquivo json
+# função para criar um novo registro em um arquivo json
 def criar_novo_registro(file_name):
     data = ler_json(file_name)
     novo = {}
@@ -31,7 +31,7 @@ def criar_novo_registro(file_name):
     if len(ids) != 0:
         id = str(max(ids) + 1)
     colunas = eval(file_name)
-    print('INCLUSÃƒO', file_name, '\n')
+    print('INCLUSÃO', file_name, '\n')
     for coluna in colunas:
         print('Informe', coluna)
         valor = input()
@@ -60,13 +60,13 @@ def ler_registro(file_name):
     return registro, identificador
 
 
-# funÃ§Ã£o para atualizar um registro em um arquivo json a partir de um ID
+# função para atualizar um registro em um arquivo json a partir de um ID
 def atualizar_registro(file_name):
     data = ler_json(file_name)
-    print('ATUALIZAÃ‡ÃƒO', file_name, '\n')
+    print('ATUALIZAÇÃO', file_name, '\n')
     registro, identificador = ler_registro(file_name)
     if registro is None or identificador is None:
-        print('O ID do registro nÃ£o pode ser nulo!')
+        print('O ID do registro não pode ser nulo!')
         finalizar_programa()
     colunas = eval(file_name)
     for coluna in colunas:
@@ -77,26 +77,26 @@ def atualizar_registro(file_name):
     print('Registro {identificador} alterado!')
 
 
-# funÃ§Ã£o para remover um novo registro em um arquivo json  a partir de um ID
+# função para remover um novo registro em um arquivo json  a partir de um ID
 def remover_registro(file_name):
     data = ler_json(file_name)
-    print('EXCLUSÃƒO', file_name, '\n')
+    print('EXCLUSÃO', file_name, '\n')
     registro, identificador = ler_registro(file_name)
     if registro is None or identificador is None:
-        print('O ID do registro nÃ£o pode ser nulo!')
+        print('O ID do registro não pode ser nulo!')
     else:
-        print('Confirma a remoÃ§Ã£o do ID:', identificador, '? (s/n)\n'
-                                                            'OBS: Essa operaÃ§Ã£o nÃ£o pode ser desfeita!')
+        print('Confirma a remoção do ID:', identificador, '? (s/n)\n'
+                                                            'OBS: Essa operação não pode ser desfeita!')
         confirma = input().lower()
         if 's' in confirma:
             data.pop(identificador)
             escrever_json(data, file_name)
             print('Registro', identificador, 'removido!')
         else:
-            print('A remoÃ§Ã£o do registro:', identificador, 'foi cancelada!')
+            print('A remoção do registro:', identificador, 'foi cancelada!')
 
 
-# funÃ§Ã£o para buscar registros em um arquivo json  a partir de um texto
+# função para buscar registros em um arquivo json  a partir de um texto
 def buscar_por_coluna(file_name):
     data = ler_json(file_name)
     print('BUSCA', file_name, '\n')
@@ -122,7 +122,7 @@ def listar_registro(file_name):
     print('LISTAGEM', file_name, '\n')
     if len(data) == 0:
         print('Base vazia!')
-        # funÃ§Ã£o precisa ser complementada
+        # função precisa ser complementada
     # else:
 
     input('Tecle uma tecla para continuar ...')
@@ -142,14 +142,14 @@ def operacao(tabela):
     ativo = True
     while ativo:
         limpar_tela()
-        print('O que vocÃª deseja fazer na base', tabela, ':\n\n'
+        print('O que você deseja fazer na base', tabela, ':\n\n'
                                                           '(1) Criar novo registro.\n'
                                                           '(9) Voltar menu.\n\n'
-                                                          'FaÃ§a sua escolha: ')
+                                                          'Faça sua escolha: ')
         opcao = input().lower()
 
         if opcao not in opcoes:
-            input('OpÃ§Ã£o invÃ¡lida! Tente novamente ...')
+            input('Opção inválida! Tente novamente ...')
         else:
             limpar_tela()
             if opcao == '1':
@@ -163,28 +163,28 @@ def menu():
     ativo = True
     while ativo:
         limpar_tela()
-        opcao = input('Selecione a opÃ§Ã£o desejada:\n\n'
+        opcao = input('Selecione a opção desejada:\n\n'
                       '(1) Gerenciar estudantes.\n'
                       '(9) Sair.\n\n'
-                      'FaÃ§a sua escolha: ').lower()
+                      'Faça sua escolha: ').lower()
         if opcao in opcoes:
             if opcao == '1':
                 operacao(tabela1)
             elif opcao == '9':
                 ativo = False
             else:
-                print('OpÃ§Ã£o invÃ¡lida! Tente novamente.')
+                print('Opção inválida! Tente novamente.')
 
     finalizar_programa()
 
 
 if __name__ == '__main__':
     # definindo os nomes das colunas para a tabela estudantes
-    estudantes = ['matrÃ­cula', 'nome', 'sobrenome']
-    tabela1 = 'estudantes'  # nome da tabela deve ser o mesmo nome da variÃ¡vel
+    estudantes = ['matrícula', 'nome', 'sobrenome']
+    tabela1 = 'estudantes'  # nome da tabela deve ser o mesmo nome da variável
 
-    # Seguindo o mesmo exemplo de criaÃ§Ã£o das tabelas pode-se criar quantas
-    # forem necessÃ¡rias
+    # Seguindo o mesmo exemplo de criação das tabelas pode-se criar quantas
+    # forem necessárias
 
     # chamada principal do programa
     menu()
